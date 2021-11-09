a8374e20cb36b057a16c03d6b4ad9ee93aefd536
samuel Guzman <samuelguzman@pop-os.localdomain>
2021-11-09 17:14:53 +0100

    archivos vip

---
+++ php/IsVip.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include '../html/Head.html'?>
+    </head>
+    <body>
+    <?php include '../php/Menus.php' ?>
+    <section class="main" id="s1">
+        <h2>Cliente REST para saber si el usuario dado es VIP</h2>
+        <form action="" method="post">
+            <h1>Es VIP?</h1>
+            <input type="text">
+            <input type="submit" value="Es VIP?">
+        </form>
+        <div id="esVipResponse">
+            
+        </div>
+    </section>
+    <?php include '../html/Footer.html' ?>
+    </body>
+</html>
+<?php
+
+?>
