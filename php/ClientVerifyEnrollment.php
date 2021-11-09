e9ef81633381f8a15d78481f0488c3259a881b5b
Oier <oieruni22@gmail.com>
2021-11-09 16:39:50 +0100

    Empezando la tarea 1, comprobando el funcionamiento del SoapClient

--- php/ClientVerifyEnrollment.php
+++ php/ClientVerifyEnrollment.php
@@ -1,4 +1,6 @@
 <?php
 
-$server= new SoapServer('http://ehusw.es/jav/ServiciosWeb/comprobarmatricula.php?wsdl')or die('No se ha podido conectar al server');
-?>
+$soapclient = new SoapClient('http://ehusw.es/jav/ServiciosWeb/comprobarmatricula.php?wsdl') or die('No se ha podido conectar al server');
+$yo = 'osolabarrieta001@ikasle.ehu.eus';
+$result = $soapclient->comprobar($yo);
+echo $result;
