fbbe7007359d3edd6ddfaf9114e4a1f8e90d0260
Oier <oieruni22@gmail.com>
2021-11-10 13:52:40 +0100

    Intentando hacer que funcione el GET

--- php/IsVip.php
+++ php/IsVip.php
@@ -11,17 +11,18 @@
         <h2>Cliente REST para saber si el usuario dado es VIP</h2>
         <form action="" method="post">
             <h1>Es VIP?</h1>
-            <input type="text">
+            <input name="email" type="text">
             <input type="submit" value="Es VIP?">
         </form>
         <div id="esVipResponse">
             <?php
+            $id = $_POST['email'];
             $curl = curl_init();
-            $url = "https://localhost/formulario-api/vipusers";
+            $url = "localhost/~oier/formulario-api/php/VipUsers.php?id=" . $id;
             curl_setopt($curl, CURLOPT_URL, $url);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-            $vip = curl_exec($curl);
-            echo $vip;
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 0);
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $id);
 
             ?>
 
