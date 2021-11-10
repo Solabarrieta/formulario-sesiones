9d0bece074426722f967072d2851af685b92983d
Oier <oieruni22@gmail.com>
2021-11-10 20:52:02 +0100

    Funciona el GET para listar los usuarios VIP

---
+++ php/ShowVips.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php include '../html/Head.html' ?>
+</head>
+
+<body>
+    <?php include '../php/Menus.php' ?>
+    <section class="main" id="s1">
+        <h2>Cliente REST para mostrar un listado de usuarios VIP</h2>
+        <form action="" method="post">
+            <h1>Usuarios VIP: </h1>
+            <input name="form-button" type="submit" value="Mostrar VIPS" style="margin: 25px;">
+        </form>
+        <div id="esVipResponse" style="margin: 20px;">
+            <?php
+            if (isset($_POST['form-button'])) {
+                $curl = curl_init();
+                $url = "localhost/~oier/formulario-api/php/VipUsers.php";
+                curl_setopt($curl, CURLOPT_URL, $url);
+                curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+                curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
+                $vip = curl_exec($curl);
+
+                echo $vip;
+            }
+
+
+            ?>
+
+        </div>
+    </section>
+    <?php include '../html/Footer.html' ?>
+</body>
+
+</html>
