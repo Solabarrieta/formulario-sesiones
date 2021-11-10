df51e1b7237cd83aeafd8aebaf67b5f8b15490c3
Oier <oieruni22@gmail.com>
2021-11-10 19:51:52 +0100

    Haciendo unos arreglos al IsVip.php

--- php/IsVip.php
+++ php/IsVip.php
@@ -11,27 +11,21 @@
         <h2>Cliente REST para saber si el usuario dado es VIP</h2>
         <form action="" method="post">
             <h1>Es VIP?</h1>
-            <input name="id" type="text">
-            <input id="form-button" name="form-button" type="submit" value="Es VIP?">
+            <input name="email" type="text">
+            <input type="submit" value="Es VIP?">
         </form>
         <div id="esVipResponse">
             <?php
-            if (isset($_POST['form-button'])) {
-                $id = $_POST['id'];
-                echo $id . PHP_EOL;
-                $curl = curl_init();
-                $url = "http://localhost/~oier/formulario-api/php/VipUsers.php?id=" . $id;
-                curl_setopt($curl, CURLOPT_URL, $url);
-                curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-                //curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
-                $vip = curl_exec($curl);
+            $id = $_POST['email'];
+            $curl = curl_init();
+            $url = "localhost/~oier/formulario-api/php/VipUsers.php?id=" . $id;
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
+            $vip = curl_exec($curl);
 
-                if (empty($vip)) {
-                    echo 'No hay nada!' . PHP_EOL;
-                } else {
-                    echo $vip;
-                }
-            }
+            echo $vip;
+
             ?>
 
         </div>
