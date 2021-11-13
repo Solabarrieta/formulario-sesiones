7c01914f6f19310aa9f7d2d93e58a6ab15441ee5
samuel Guzman <samuelguzman@pop-os.localdomain>
2021-11-13 14:11:23 +0100

    menu with SQL

--- php/Menus.php
+++ php/Menus.php
@@ -22,23 +22,30 @@
       <span><a href="ShowJsonQuestionsWithImage.php?correo=<?php echo $_GET["correo"]; ?>"> Ver Preguntas JSON</a></span>
 
       <?php
-            $id = $_GET["correo"];
-            
-            $curl = curl_init();
-            $url = "http://localhost/SW-API/php/VipUsers.php?id=" . $id;
-            curl_setopt($curl, CURLOPT_URL, $url);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
-            $vip = curl_exec($curl);
-            $string= "ENHORABUENA";
-      
-      if(strpos($vip,$string)){ ?>
+          DEFINE("_HOST_", "localhost");
+          DEFINE("_PORT_", "3306");
+          DEFINE("_USERNAME_", "root");
+          DEFINE("_DATABASE_", "db_G22");
+          DEFINE("_PASSWORD_", "2000");
+          $id = $_GET["correo"];
+          
+          require_once 'database.php';
+          $cnx = Database::Conectar();
+          $sql = "SELECT * FROM users WHERE tipouser='prof' and correo='$id'";
+          $data = Database::EjecutarConsulta($cnx, $sql);
+          if (!empty($data)) {?>
         <span><a href="IsVip.php?correo=<?php echo $_GET["correo"]; ?>"> EsVip?</a></span>
         <span><a href="AddVipUser.php?correo=<?php echo $_GET["correo"]; ?>">Añadir usuario VIP</a></span>
         <span><a href="DeleteVipUser.php?correo=<?php echo $_GET["correo"]; ?>">Eliminar Usuario VIP</a></span>
         <span><a href="ShowVips.php?correo=<?php echo $_GET["correo"]; ?>">Mostrar VIPS</a></span>
+        <?php
+        } 
 
 
+          $er2 = "/^[a-zA-Z]+\.[a-zA-Z]+@ehu\.(eus|es)$/";
+          $er3 = "/^[a-zA-Z]+@ehu\.(eus|es)$/";
+
+      if(preg_match($er2,$id)||preg_match($er3,$id)){ ?>
       <?php } ?>
       
 
