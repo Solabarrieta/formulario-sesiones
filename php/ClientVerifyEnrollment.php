aae9bb3b2208b2f6e63ee7625f87612065b79a33
Oier <oieruni22@gmail.com>
2021-11-13 16:10:01 +0100

    Quitando echos

--- php/ClientVerifyEnrollment.php
+++ php/ClientVerifyEnrollment.php
@@ -1,7 +1,6 @@
 <?php
 if (isset($correo)) {
     $soapClient = new SoapClient('http://ehusw.es/jav/ServiciosWeb/comprobarmatricula.php?wsdl');
-    echo $correo;
     $response = $soapClient->comprobar($correo);
     $valido = False;
     if ($response == 'SI') {
