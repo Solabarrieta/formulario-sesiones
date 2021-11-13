235877656168de24993237ae469dedbb202afd41
Oier <oieruni22@gmail.com>
2021-11-13 15:45:53 +0100

    Haciendo que el DELETE solo funcione cuando se clica el boton submit

--- php/DeleteVipUser.php
+++ php/DeleteVipUser.php
@@ -16,15 +16,18 @@
         </form>
         <div id="esVipResponse">
             <?php
-            $id = $_POST['email'];
-            $url = "https://sw.ikasten.io/~G22/formulario-api/php/VipUsers.php";
-            $curl = curl_init();
-            curl_setopt($curl, CURLOPT_URL, $url);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'DELETE');
-            curl_setopt($curl, CURLOPT_POSTFIELDS, $id);
-            $vip = curl_exec($curl);
-            echo $vip;
+            if (isset($_POST['email'])) {
+                $id = $_POST['email'];
+                $url = "https://sw.ikasten.io/~G22/formulario-api/php/VipUsers.php";
+                $curl = curl_init();
+                curl_setopt($curl, CURLOPT_URL, $url);
+                curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+                curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'DELETE');
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $id);
+                $vip = curl_exec($curl);
+                echo $vip;
+            }
+
             ?>
 
         </div>
