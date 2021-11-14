d16626de74677974f1eaf12e467f38faa3e58d4a
Oier <oieruni22@gmail.com>
2021-11-14 23:46:59 +0100

    Resolviendo errores de ultima hora, parametros de la base de datos no definidos correctamente

--- php/Menus.php
+++ php/Menus.php
@@ -1,7 +1,7 @@
 <div id='page-wrap'>
   <header class='main' id='h1'>
     <?php if (!isset($_GET['correo'])) { ?>
-      
+
 
 
       <span class="right"><a href="SignUp.php">Registro</a></span>
@@ -13,7 +13,7 @@
   </header>
   <nav class='main' id='n1' role='navigation'>
     <?php if (isset($_GET['correo'])) { ?>
-      
+
       <span><a href="Layout.php?correo=<?php echo $_GET["correo"]; ?>"> Inicio</a></span>
       <span><a href="Credits.php?correo=<?php echo $_GET["correo"]; ?>">Creditos</a></span>
       <span><a href="HandlingQuizesAjax.php?correo=<?php echo $_GET["correo"]; ?>"> Insertar Pregunta </a></span>
@@ -22,32 +22,32 @@
       <span><a href="ShowJsonQuestionsWithImage.php?correo=<?php echo $_GET["correo"]; ?>"> Ver Preguntas JSON</a></span>
 
       <?php
-          DEFINE("_HOST_", "localhost");
-          DEFINE("_PORT_", "3306");
-          DEFINE("_USERNAME_", "root");
-          DEFINE("_DATABASE_", "db_G22");
-          DEFINE("_PASSWORD_", "2000");
-          $id = $_GET["correo"];
-          
-          require_once 'database.php';
-          $cnx = Database::Conectar();
-          $sql = "SELECT * FROM users WHERE tipouser='prof' and correo='$id'";
-          $data = Database::EjecutarConsulta($cnx, $sql);
-          if (!empty($data)) {?>
+      DEFINE("_HOST_", "localhost");
+      DEFINE("_PORT_", "3306");
+      DEFINE("_USERNAME_", "G22");
+      DEFINE("_DATABASE_", "db_G22");
+      DEFINE("_PASSWORD_", "TWTnlYm33HtAL");
+      $id = $_GET["correo"];
+
+      require_once 'database.php';
+      $cnx = Database::Conectar();
+      $sql = "SELECT * FROM users WHERE tipouser='prof' and correo='$id'";
+      $data = Database::EjecutarConsulta($cnx, $sql);
+      if (!empty($data)) { ?>
         <span><a href="IsVip.php?correo=<?php echo $_GET["correo"]; ?>"> EsVip?</a></span>
         <span><a href="AddVipUser.php?correo=<?php echo $_GET["correo"]; ?>">Añadir usuario VIP</a></span>
         <span><a href="DeleteVipUser.php?correo=<?php echo $_GET["correo"]; ?>">Eliminar Usuario VIP</a></span>
         <span><a href="ShowVips.php?correo=<?php echo $_GET["correo"]; ?>">Mostrar VIPS</a></span>
-        <?php
-        } 
+      <?php
+      }
 
 
-          $er2 = "/^[a-zA-Z]+\.[a-zA-Z]+@ehu\.(eus|es)$/";
-          $er3 = "/^[a-zA-Z]+@ehu\.(eus|es)$/";
+      $er2 = "/^[a-zA-Z]+\.[a-zA-Z]+@ehu\.(eus|es)$/";
+      $er3 = "/^[a-zA-Z]+@ehu\.(eus|es)$/";
 
-      if(preg_match($er2,$id)||preg_match($er3,$id)){ ?>
+      if (preg_match($er2, $id) || preg_match($er3, $id)) { ?>
       <?php } ?>
-      
+
 
     <?php } else { ?>
       <span><a href="Layout.php"> Inicio</a></span>
