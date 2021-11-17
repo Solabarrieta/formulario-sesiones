46917f073dab14557920e5d281ff561899b3a58a
Oier <oieruni22@gmail.com>
2021-11-17 18:02:35 +0100

    Codigo php para cambiar el estado del usuario terminado

---
+++ php/ChangeStatus.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['correo'])) {
+    echo '<script type="text/javascript"> alert("Debe debes ser ADMINISTRADOR para estar en esta pagina!! ");
+    window.location.href="LogIn.php";
+    </script>';
+}
+
+
+
+DEFINE("_HOST_", "localhost");
+DEFINE("_PORT_", "3306");
+DEFINE("_USERNAME_", "oier");
+DEFINE("_DATABASE_", "Quiz");
+DEFINE("_PASSWORD_", "4258");
+
+
+require_once 'database.php';
+
+
+
+
+
+$cnx = Database::Conectar();
+
+
+if (isset($_POST['email'], $_POST['state'])) {
+
+    echo '<script>alert(\'' . $_POST . '\')</script>';
+
+    $correo = $_POST['email'];
+    $estado = $_POST['state'];
+
+    $sql = "UPDATE users SET estado = '$estado' WHERE correo = '$correo'";
+    Database::EjecutarNoConsulta($cnx, $sql);
+}
+
+Database::Desconectar($cnx);
