0fd74a832d2b1b2064063cb293d84318da1b8114
Oier <oieruni22@gmail.com>
2021-11-17 18:02:06 +0100

    Creando una funcion para printear el contrario del estado del usuario, si es activo sale banear y si es baneado sale activar, de esta forma se evita tener que hacer un if en php para mandar el contrario del estado en sql

--- js/handleUsers.js
+++ js/handleUsers.js
@@ -1,16 +1,38 @@
 function deleteUser(correo){
-    $.ajax({
-        type: "POST",
-        url: '../php/DeleteUser.php',
-        data: {email: correo }
-    });
+    if(confirm(`¿Seguro que quieres borrar a ${correo} ?`)){
+        $.ajax({
+            type: "POST",
+            url: '../php/DeleteUser.php',
+            data: {email: correo }
+        });
+    }
+}
+
+function printStatus(estado){
+    let notStatus = [];
+
+    if(estado == 'activo'){
+        notStatus.push('banear');
+        notStatus.push('baneado');
+    } else{
+        notStatus.push('activar');
+        notStatus.push('activo')
+    }
+    return notStatus;
 }
 
 
 function changeStatus(correo, estado){
-    $.ajax({
-        type: "POST",
-        url: '../php/ChangeStatus.php',
-        data: {state: estado, email: correo }
-    });
+    let notStatus = printStatus(estado);
+    alert(notStatus[0]);
+    alert(notStatus[1]);
+    let status = notStatus[1];
+    if(confirm(`¿Seguro que quieres ${notStatus[0]} a ${correo}?`)){
+        alert(status);
+        $.ajax({
+            type: "POST",
+            url: '../php/ChangeStatus.php',
+            data: {state: status, email: correo }
+        });
+    }
 }
