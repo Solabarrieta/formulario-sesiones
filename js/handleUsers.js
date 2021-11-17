8538bb2dff2c59f5f45368a6371ff19d16f597fd
Oier <oieruni22@gmail.com>
2021-11-17 12:13:13 +0100

    Creado el script para mandarle al php los parametros para ejecutar las funcionalidades de borrar usuario y cambiar su estado, no funciona correctamente

---
+++ js/handleUsers.js
@@ -0,0 +1,16 @@
+function deleteUser(correo){
+    $.ajax({
+        type: "POST",
+        url: '../php/DeleteUser.php',
+        data: {state: estado, email: correo }
+    });
+}
+
+
+function changeState(correo, estado){
+    $.ajax({
+        type: "POST",
+        url: '../php/HandlingAccounts.php',
+        data: {state: estado, email: correo }
+    });
+}
